415efaf216d352df51fbbd0e13664a02dcf5455f
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2022-01-08 22:42:28 +0300

    Added Login Page model and tests

---
+++ test_login_page.py
@@ -0,0 +1,18 @@
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+
+def test_login_page_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_login_page_should_be_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_login_page_should_be_registration_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_registration_form()
