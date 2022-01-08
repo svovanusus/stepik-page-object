a01d30c2674b1247bd47d41bf45ff603e9f6964a
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2022-01-08 21:58:46 +0300

    Added logic to understandable errors

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser: WebDriver, url: str, timeout:int=10):
+
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+    
+    def is_element_present(self, how: By, what: str):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
 from .pages.main_page import MainPage
 
+def test_guest_should_see_ligin_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # Page Object init
