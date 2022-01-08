92b27a1e3fda9717b3bb6f785611fc2952d67194
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2022-01-09 00:45:16 +0300

    Refactored test-case file for Product Page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,5 @@
 from .pages.product_page import ProductPage
 from selenium.webdriver.remote.webdriver import WebDriver
-import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -12,4 +11,3 @@ def test_quest_can_add_product_to_basket(browser: WebDriver):
     page.add_to_basket()
     page.should_be_add_to_basket_alert()
     page.should_be_basket_state_alert()
-    #time.sleep(3600)
