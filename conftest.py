a279367d8a100fa75867b32e6005cb08dcb62f5d
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2022-01-08 22:45:16 +0300

    Fixed code style

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action="store", default="en", help="Choose browser language")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 from .pages.main_page import MainPage
 
+
 link = "http://selenium1py.pythonanywhere.com/"
+
 
 def test_guest_should_see_ligin_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # Page Object init
