092400e52bcca41876a049d3ca01d1ce6389f262
Vlavimir Solodovnikov <svovanusus@inbox.ru>
2022-01-09 04:07:22 +0300

    Refactored project: Final changes to review

--- test_product_page.py
+++ test_product_page.py
@@ -22,6 +22,7 @@ class TestUserAddToBasketFromProductPage():
         page.should_not_be_success_message()
 
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser: WebDriver):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
@@ -34,7 +35,7 @@ class TestUserAddToBasketFromProductPage():
 @pytest.mark.parametrize("promo", ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
                                 pytest.param("offer7", marks=pytest.mark.xfail),
                                 "offer8", "offer9"])
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser: WebDriver, promo: str):
     page = ProductPage(browser, f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo}")
     page.open()
@@ -43,7 +44,6 @@ def test_guest_can_add_product_to_basket(browser: WebDriver, promo: str):
     page.should_be_basket_state_alert()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -51,7 +51,7 @@ def test_guest_should_see_login_link_on_product_page(browser: WebDriver):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -61,7 +61,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser: WebDriver):
     login_page.should_be_login_page()
 
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -70,7 +69,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser: 
     page.should_not_be_success_message()
 
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -78,7 +76,6 @@ def test_guest_cant_see_success_message(browser: WebDriver):
     page.should_not_be_success_message()
 
 
-@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -87,7 +84,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser: WebDriver):
     page.should_be_disappeared_success_message()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
